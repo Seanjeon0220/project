14197894ae749aba28b91fd2820968dc6d01939a
Sean <seanjeon220@gmail.com>
2026-03-19 15:40:52 +0800

    algorithm_0318

---
+++ algorithm/math/P3449.py
@@ -0,0 +1,9 @@
+T = int(input())
+
+for _ in range(T):
+    a = input(); b = input()
+    cnt = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            cnt += 1
+    print(f"Hamming distance is {cnt}.")
